21ef2af0cddeaa6b44cfc3be1037d65070f08e88
Godwin K <koffigodwin96@gmail.com>
2024-04-19 18:52:27 +0200

    fix:Add classmap

--- src/PDFWrapper.php
+++ src/PDFWrapper.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace Drsi\FPDF;
 
-use FPDF;
+// use FPDF;
 
 if (class_exists('setasign\Fpdi\Fpdi')) {
     class PDFWrapper extends \setasign\Fpdi\Fpdi {};
 } else {
-    class PDFWrapper extends FPDF {};
+    class PDFWrapper extends \FPDF {};
 }
